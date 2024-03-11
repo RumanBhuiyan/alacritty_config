03bc454b663fc06436786d6fc65fa191c60142c6
RumanBhuiyan <rumanbhuiyansany@gmail.com>
2024-03-11 11:15:32 +0600

    update installer script after testing in my end

--- installer.sh
+++ installer.sh
@@ -8,7 +8,11 @@ if [ -d ~/.config/alacritty/ ];then
 fi
 
 # step - 02: clone the github repo into userspace home directory first
-git clone git@github.com:RumanBhuiyan/alacritty_config.git ~/
+if [ -d ~/alacritty_config ]; then 
+  echo "Removing ~/alacritty_config directory "
+  rm -rf ~/alacritty_config
+fi 
+git clone https://github.com/RumanBhuiyan/alacritty_config.git ~/alacritty_config
 
 # step - 03 : copy the alacritty executable binary file into ~/.local/bin folder
 if [ ! -d ~/.local/bin/ ];then 
@@ -16,15 +20,20 @@ if [ ! -d ~/.local/bin/ ];then
   mkdir -m777 ~/.local/bin
 fi
 
+echo "Copying alacritty executable binary into ~/.local/bin folder !"
 cp -v ~/alacritty_config/alacritty ~/.local/bin/
 
 # step - 04 : now copy the alacritty folder into ~/.config folder 
-if [ ! -d ~/.config/ ];then
-  echo "~/.config/ doesn't exist. Creating..."
-  mkdir -m777 ~/.config/
+if [ ! -d ~/.config/alacritty ];then
+  echo "~/.config/alacritty doesn't exist. Creating..."
+  mkdir -m777 ~/.config/alacritty
 fi 
-cp -v ~/alacritty_config/alacritty.toml ~/.config/alacritty 
-cp -rv ~/alacritty_config/themes ~/.config/alacritty/
+
+echo "Copying alacritty config toml file..."
+cp -v ~/alacritty_config/alacritty.toml ~/.config/alacritty/
+
+echo "Copying alacritty themes into ~/.config/alacritty/themes folder..."
+cp -r ~/alacritty_config/themes ~/.config/alacritty/themes/
 
 # step - 05 : add the executable binary path to PATH if doesn't exist
 if [[ ! $PATH == *"/.local/bin/"* ]];then 
@@ -33,5 +42,8 @@ if [[ ! $PATH == *"/.local/bin/"* ]];then
 fi 
 
 # step - 06 : remove the github repo 
+echo "Removing ~/alacritty_config git repo from home directory."
 rm -rf ~/alacritty_config 
+
+echo "Alacritty installed... type alacritty and hit enter!!"
 
