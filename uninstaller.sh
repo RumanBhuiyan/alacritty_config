a8c6b8cf75b7cd91f6a898b7f62646471c79169a
RumanBhuiyan <rumanbhuiyansany@gmail.com>
2024-03-11 11:43:49 +0600

    update uninstaller script

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # step - 01 : remove the executable binary file first 
-if [ -e ~/.local/bin/alacritty ];
+if [ -e ~/.local/bin/alacritty ]; then 
   echo "removing alacritty binary file from ~/.local/bin"
   rm ~/.local/bin/alacritty
-then 
+fi
 
 # step - 02: remove alacritty configurations folder
-if [ -d ~/.config/alacritty/ ];then
+if [ -d ~/.config/alacritty ];then
   echo "Removing ~/.config/alacritty/ folder"
   rm -rf ~/.config/alacritty
 fi
+
+# step -03: also remove github repo if found in ~/ 
+if [ -d ~/alacritty_config ];then 
+  echo "Removing ~/alacritty_config folder..."
+  rm -rf ~/alacritty_config
+fi 
+
+echo "Alcritty uninstalled successfully!!!"
 
